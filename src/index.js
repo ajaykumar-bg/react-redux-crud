cd25cd8996397620ce35a4e1303b5b9299466cd8
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2019-05-15 16:13:34 +0530

    Added provider to store to make out application a redux app

--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { createStore} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
 
 const store = createStore(
-    rootReducer
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    )
+    
 )
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
