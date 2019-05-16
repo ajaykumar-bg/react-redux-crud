c88fb48c9a3607343bddaa5372c52c8c031e04de
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2019-05-16 11:57:26 +0530

    Added fetch skills action and reducer

--- src/actions/SkillsAction.js
+++ src/actions/SkillsAction.js
@@ -1,8 +1,19 @@
 import ApiConstants from '../constants/ApiConstants'
+import { SET_SKILLS } from '../constants/Contants'
+// export const SET_SKILLS = 'SET_SKILLS'
+
+export function setSkills(skills) {
+    return {
+        type: SET_SKILLS,
+        skills
+    }
+} 
 
 export function fetchSkills() {
     const { baseURL, SKILL_URL } = ApiConstants;
     return dispatch => {
         fetch(`${baseURL}${SKILL_URL}`)
+        .then(res => res.json())
+        .then(data => dispatch(setSkills(data)));
     }
 }
--- src/reducers/skills.js
+++ src/reducers/skills.js
@@ -1,5 +1,10 @@
+import { SET_SKILLS } from '../constants/Contants'
+
 export default function skills(state = [], action = {}) {
     switch (action.type) {
+        case SET_SKILLS: {
+            return action.skills;
+        }
         default: return state;
     }
 };
