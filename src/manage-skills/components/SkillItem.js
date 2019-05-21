1b6b63a65126593e705a04b9c9659b860e34ebfa
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2019-05-21 14:58:22 +0530

    Updated UI

--- src/manage-skills/components/SkillItem.js
+++ src/manage-skills/components/SkillItem.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function SkillItem({item, deleteSkill}) {
   return (
     <li className="collection-item">
-      <span>{ item.name } : {item.version}</span>
+      <span>{ item.name }({item.version})</span>
       <div className="secondary-content">
         <button><Link to={`/skills/edit/${item.id}`}><i className="fa fa-pencil right"></i>Edit</Link></button>
         <button onClick={() => deleteSkill(item.id)}><i className="fa fa-trash right"></i>Delete</button>
