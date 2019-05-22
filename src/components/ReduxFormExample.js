e49840a0e832f0a637792cacbb2331f2260bd878
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2019-05-22 17:36:30 +0530

    Handling Form Submission

--- src/components/ReduxFormExample.js
+++ src/components/ReduxFormExample.js
@@ -2,23 +2,30 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 class ReduxFormExample extends Component {
+
+    onSubmit = (formValues) => {
+        console.log(formValues)
+    }
     renderTextInput = (formProps) => {
         return (
-            <input {...formProps.input} ></input>
+            <div className="input-field col s12">
+                <input {...formProps.input} className="validate" placeholder={formProps.placeholder}></input>
+            </div>
         )
     }
-    renderTextAreaInput = (formProps) => {
+    renderTextAreaInput = ({input}) => {
         return (
-            <textarea {...formProps.input} ></textarea>
+            <textarea {...input} ></textarea>
         )
     }
     render() {
         return (
-            <div>
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
                 <h3>Redux Form Example</h3>
-                <Field name="title" component={this.renderTextInput}/>
-                <Field name="description" component={this.renderTextAreaInput}/>
-            </div>
+                <Field name="title" component={this.renderTextInput} placeholder="Enter title"/>
+                <Field name="description" component={this.renderTextAreaInput} placeholder="Enter description"/>
+                <button className="btn" type="submit">Submit</button>
+            </form>
         )
     }
 }
